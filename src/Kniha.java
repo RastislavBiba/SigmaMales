23349ff79dbadffbe3a14877ac416ac7228bc51c
Rastislav Biba <bibarastislav05@gmail.com>
2022-02-09 12:33:23 +0100

    Project with Updated class main and added class Kniha where is script for adding book in library system

---
+++ src/Kniha.java
@@ -0,0 +1,19 @@
+public class Kniha {
+    String Nazov;
+    String Autor;
+    int PocetStran;
+    int AktualnaStrana;
+
+    public void setNazov(String nazov){
+            this.Nazov = nazov;
+        }
+    public void setAutor(String autor){
+            this.Autor = autor;
+        }
+    public void setPocetStran(int pocetstran){
+            this.PocetStran = pocetstran;
+        }
+    public void setAktualnaStrana(int aktualnaStrana){
+            this.AktualnaStrana = aktualnaStrana;
+        }
+}
